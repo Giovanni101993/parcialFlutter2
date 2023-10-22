f007c57aab85298c5d119a263b67493c14b7f137
Giovanni Cobo <giovanni.cobo10@gmail.com>
2023-10-22 14:23:00 -0500

    Se agrega la animacion

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,15 +1,73 @@
 import 'package:flutter/material.dart';
 
-class DashBoard extends StatelessWidget{
-  const DashBoard({Key?key}) : super(key:key);
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DashBoard(),
+    );
+  }
+}
+
+class DashBoard extends StatefulWidget {
+  const DashBoard({Key? key}) : super(key: key);
+
+  @override
+  _DashBoardState createState() => _DashBoardState();
+}
+
+class _DashBoardState extends State<DashBoard> {
+  int _currentColorIndex = 0;
+  List<Color> _colors = [
+    Colors.red,
+    Colors.green,
+    Colors.blue,
+    Colors.yellow,
+    Colors.orange,
+    Colors.purple,
+    Colors.teal,
+    Colors.pink,
+    Colors.indigo,
+    Colors.cyan,
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Iniciar el ciclo de cambio de colores
+    startColorCycle();
+  }
+
+  void startColorCycle() {
+    Future.delayed(Duration(seconds: 1), () {
+      if (_currentColorIndex < _colors.length - 1) {
+        _currentColorIndex++;
+      } else {
+        _currentColorIndex = 0;
+      }
+
+      // Llamar a setState para redibujar la pantalla con el nuevo color
+      setState(() {
+        startColorCycle();
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Dashboard"),
+        title: Text("Dashboard"),
       ),
-      body: const Center(
-        child: Text("DashBoard"),
+      body: AnimatedContainer(
+        duration: Duration(seconds: 1),
+        color: _colors[_currentColorIndex],
+
       ),
     );
   }
