f40c0c6ed7e7085c66cd363374c4bfba555ccec4
Giovanni Cobo <giovanni.cobo10@gmail.com>
2023-10-22 12:17:45 -0500

    Corrijo los archivos php

--- bd_flutter/login.php
+++ bd_flutter/login.php
@@ -1,15 +1,39 @@
 <?php
 require_once("db.php");
 
-$email = $_POST['email'];
-$password = $_POST['password'];
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    // Si la solicitud no es un POST, muestra un mensaje de error.
+    echo json_encode("Método no permitido");
+    exit;
+}
 
-$sql = "SELECT * FROM usuarios WHERE email = '".$email."' AND password = md5('".$password."')";
-$result = mysqli_query($db, $sql);
-$count = mysqli_num_rows($result);
-if ($count == 1) {
-    echo json_encode("Success");
-}else{
-    echo json_encode("Error");
+// Obtiene los datos del formulario POST
+$email = isset($_POST['email']) ? $_POST['email'] : '';
+$password = isset($_POST['password']) ? $_POST['password'] : '';
+
+if (empty($email) || empty($password)) {
+    // Si falta el correo electrónico o la contraseña, muestra un mensaje de error.
+    echo json_encode("Faltan datos obligatorios");
+} else {
+    // Escapa los valores para proteger contra la inyección SQL
+    $email = mysqli_real_escape_string($db, $email);
+    $password = mysqli_real_escape_string($db, $password);
+
+    // Realiza la consulta utilizando una consulta preparada
+    $query = "SELECT * FROM usuarios WHERE email = ? AND password = MD5(?)";
+    $stmt = mysqli_prepare($db, $query);
+    mysqli_stmt_bind_param($stmt, "ss", $email, $password);
+    mysqli_stmt_execute($stmt);
+
+    // Obtiene el resultado de la consulta
+    $result = mysqli_stmt_get_result($stmt);
+
+    if (mysqli_num_rows($result) == 1) {
+        // Si se encontró una fila en la base de datos, el inicio de sesión es exitoso.
+        echo json_encode("Success");
+    } else {
+        // Si no se encontró una fila, muestra un mensaje de error.
+        echo json_encode("Error");
+    }
 }
 ?>
--- bd_flutter/register.php
+++ bd_flutter/register.php
@@ -1,27 +1,45 @@
 <?php
-
 require_once('db.php');
 
-if(!$db){
-    echo 'conection failed';
-}
+if (!$db) {
+    echo 'Connection failed';
+} else {
+    // Verifica si se están enviando datos POST
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $name = $_POST['name'] ?? '';
+        $email = $_POST['email'] ?? '';
+        $phone = $_POST['phone'] ?? '';
+        $password = $_POST['password'] ?? '';
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$password = $_POST['password'];
+        // Verifica si los datos requeridos no están vacíos
+        if (!empty($name) && !empty($email) && !empty($password)) {
+            // Verifica si el correo electrónico ya existe en la base de datos
+            $query = "SELECT email FROM usuarios WHERE email = ?";
+            $stmt = $db->prepare($query);
+            $stmt->bind_param('s', $email);
+            $stmt->execute();
+            $stmt->store_result();
 
-$sql = "SELECT email FROM usuarios WHERE email = '".$email."'";
-$result = mysqli_query($db, $sql);
-$count = mysqli_num_rows($result);
-if($count == 1){
-    echo json_encode('Error');
-}else{
-    $insert = "INSERT INTO usuarios(name, email, phone, password) VALUES ('".$name."', '".$email."',
-    '".$phone."', md5('".$password."'))";
-    $query = mysqli_query($db, $insert);
-    if($query){
-        echo json_encode("Success");
+            if ($stmt->num_rows == 0) {
+                // El correo electrónico no existe, inserta un nuevo registro
+                $query = "INSERT INTO usuarios (name, email, phone, password) VALUES (?, ?, ?, ?)";
+                $stmt = $db->prepare($query);
+                $hashedPassword = md5($password);
+                $stmt->bind_param('ssss', $name, $email, $phone, $hashedPassword);
+
+                if ($stmt->execute()) {
+                    echo json_encode("Success");
+                } else {
+                    echo json_encode("Error al registrar el usuario");
+                }
+            } else {
+                echo json_encode("El correo electrónico ya existe");
+            }
+        } else {
+            echo json_encode("Faltan datos obligatorios");
+        }
+    } else {
+        echo json_encode("Método no permitido");
     }
 }
 ?>
